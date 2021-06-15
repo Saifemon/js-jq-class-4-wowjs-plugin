d6adb5f9c7b291b21722cc57f2a90b2f302bec23
Saif Emon <74553895+Saifemon@users.noreply.github.com>
2021-06-15 20:18:04 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+});
+
+//
+wow = new WOW(
+  {
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+}
+)
+wow.init();
+
+//
+wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'orange wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
